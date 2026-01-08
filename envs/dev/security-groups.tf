8fd6804c7cd199b5928e780f3cbd06d61a873072
Danilburov <499732@student.fontys.nl>
2026-01-08 15:55:33 +0100

    Fix: Typo in ingress for the new SG

--- envs/dev/security-groups.tf
+++ envs/dev/security-groups.tf
@@ -30,12 +30,13 @@ resource "aws_security_group" "dtap-backend-alb-sg"{
   description = "SG for the backend ALB"
   vpc_id = aws_vpc.main.id
 
-  ingress = {
+  ingress {
     description = "HTTP"
     from_port = 80
     to_port = 80
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
   egress {
     from_port = 0
