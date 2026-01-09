e0611b45da6f79d4215c2b89f0eb4936214c9128
Danilburov <499732@student.fontys.nl>
2026-01-09 16:16:09 +0100

    Add: Created the ECS services with IaC

--- envs/dev/ecs-service.tf
+++ envs/dev/ecs-service.tf
@@ -1,23 +1,47 @@
-# resource "aws_ecs_service" "dtap-backend-dev" {
-#   name = "dtap-backend-dev"
-#   cluster = aws_ecs_cluster.backend.arn
-#   task_definition = aws_ecs_task_definition.iac-dtap-backend-dev.arn
-#   desired_count = 1
-#   launch_type = "FARGATE"
+resource "aws_ecs_service" "dtap-backend-dev" {
+  name = "dtap-backend-dev"
+  cluster = aws_ecs_cluster.backend.arn
+  task_definition = aws_ecs_task_definition.iac-dtap-backend-dev.arn
+  desired_count = 1
+  launch_type = "FARGATE"
 
-#   network_configuration {
-#     subnets = var.private_subnet_ids
-#     security_groups = [aws_security_group.backend_tasks_sg.id]
-#     assign_public_ip = false
-#   }
+  network_configuration {
+    subnets = [aws_subnet.public_a.id, aws_subnet.public_b.id]
+    security_groups = [aws_security_group.ecs_service_backend_sg.id]
+    assign_public_ip = true
+  }
 
-#   load_balancer {
-#     target_group_arn = aws_alb_target_group.dtap-backend-tg.arn
-#     container_name = "backend"
-#     container_port = 8080
-#   }
+  load_balancer {
+    target_group_arn = aws_alb_target_group.dtap-backend-tg.arn
+    container_name = "backend"
+    container_port = 8080
+  }
 
-#   lifecycle {
-#     ignore_changes = [task_definition]
-#   }
-# }
+  lifecycle {
+    ignore_changes = [task_definition]
+  }
+}
+resource "aws_ecs_service" "dtap-frontend-dev" {
+  name = "dtap-frontend-dev"
+  cluster = aws_ecs_cluster.frontend.arn
+  task_definition = aws_ecs_task_definition.iac-dtap-frontend-dev.arn
+  desired_count = 1
+  launch_type = "FARGATE"
+
+  network_configuration {
+    subnets = [aws_subnet.public_a.id, aws_subnet.public_b.id]
+    security_groups = [aws_security_group.ecs_service_frontend_sg.id]
+    assign_public_ip = true
+  }
+
+  load_balancer {
+    target_group_arn = aws_alb_target_group.dtap-backend-tg.arn
+    container_name = "frontend"
+    container_port = 80
+  }
+
+  lifecycle {
+    ignore_changes = [task_definition]
+  }
+}
+
