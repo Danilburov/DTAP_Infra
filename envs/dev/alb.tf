2956fc6f8cc42567535295a50e98b84c9ed413a3
Danilburov <499732@student.fontys.nl>
2026-01-09 15:26:59 +0100

    Fix: revert changes

--- envs/dev/alb.tf
+++ envs/dev/alb.tf
@@ -97,7 +97,7 @@ resource "aws_lb_target_group" "dtap-frontend-tg"{
   } 
 }
 resource "aws_alb_listener" "dtap_frontend-http" {
-  load_balancer_arn = aws_lb.dtap-app_alb.arn
+  load_balancer_arn = aws_lb.app_alb.arn
   port = 80
   protocol = "HTTP"
   
